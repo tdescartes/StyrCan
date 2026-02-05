e71ef42edc52ce3ef3e40b11298eca682302f27a
tdescartes <descartes.tuyishime1@marist.edu>
2026-02-05 16:47:58 -0600

    fix(config): update basePath and assetPrefix in next.config.ts for production environment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "export",
   trailingSlash: true,
-  basePath: "/StyrCan",
+  basePath: isProd ? "/StyrCan" : "",
+  assetPrefix: isProd ? "/StyrCan" : "",
   images: {
     unoptimized: true,
   },
