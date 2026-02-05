acb316fc2df3498e3cee1a0f6306756a5b05fb1a
tdescartes <descartes.tuyishime1@marist.edu>
2026-02-04 19:34:47 -0600

    chore(routers): export messaging and notifications routers

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,5 +1,7 @@
 """API routers package."""
 
 from .auth import router as auth_router
+from .messaging import router as messaging_router
+from .notifications import router as notifications_router
 
-__all__ = ["auth_router"]
+__all__ = ["auth_router", "messaging_router", "notifications_router"]
